b0ed6870a89d0f4bb739e4aca67e6633ba5d3061
James Earl Douglas <james@earldouglas.com>
2015-12-03 12:06:25 -0800

    Add tests for Validation

--- src/main/scala/Validation.scala
+++ src/main/scala/Validation.scala
@@ -1,10 +1,6 @@
 package com.udacity.github.oauth
 
 sealed trait Validation[+A] {
-  def map[B](f: A => B): Validation[B] = this match {
-    case Valid(value) => Valid(f(value))
-    case Invalid(errors) => Invalid(errors)
-  }
   def ap[B](fV: Validation[A => B]): Validation[B] = this match {
     case Valid(value) => fV match {
       case Valid(f) => Valid(f(value))
